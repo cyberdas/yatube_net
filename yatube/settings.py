07ed57d28455464681fd86b28dcb97b5368a5057
cyberdas <46424157+cyberdas@users.noreply.github.com>
2020-03-14 17:22:01 +0300

    Update settings.py

--- yatube/settings.py
+++ yatube/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     "django.contrib.flatpages",
     'sorl.thumbnail',
-    "debug_toolbar",   
 ]
 
 MIDDLEWARE = [
@@ -57,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -157,7 +155,3 @@ CACHES = {
                 'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
         }
 }
-
-INTERNAL_IPS = [
-        "127.0.0.1",
-]
