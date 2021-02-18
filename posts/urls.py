11875011cf69611b006881edb73e8236b1b85e05
Daniil Lapshenkov <lapshenkov.96@mail.ru>
2021-02-18 16:51:55 +0300

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path("new/", views.new_post, name="new_post"),
     path("group/<slug:slug>/", views.group_posts, name="group_posts"),
     path("follow/", views.follow_index, name="follow_index"),
-    # Профайл пользователя
     path("<username>/", views.profile, name="profile"),
-    # Просмотр записи
     path("<username>/<int:post_id>/", views.post_view, name="post"),
     path("<username>/<int:post_id>/edit/", views.post_edit, name="post_edit"),
     path("<username>/<int:post_id>/comment/", views.add_comment, name="add_comment"),
