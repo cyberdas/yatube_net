133a4c4e550969333118504e1b13d5d2d772b7a4
Daniil Lapshenkov <lapshenkov.96@mail.ru>
2021-02-18 16:50:10 +0300

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model() # для работы с пользователями
+User = get_user_model()
 
 
 class Group(models.Model):
@@ -12,17 +12,16 @@ class Group(models.Model):
     def __str__(self):
         return self.title 
 
-class Post(models.Model): # класс Post, наследник класса Model из библиотеки models
-    text = models.TextField() # свойство pub_date типа DateTimeField, текст "date published" это заголовок
-    pub_date = models.DateTimeField("date published", auto_now_add=True) # поля в интерфейсе администратора. auto_now_add говорит, что при создании новой записи автоматически будет подставлено текущее время и дата 
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_author")
     group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True, related_name="posts")
     image = models.ImageField(upload_to="posts/", blank=True, null=True)
 
-    def __str__(self): # выводим текст поста  
+    def __str__(self):
         return self.text 
-# Параметр on_delete=models.CASCADE обеспечивает связность данных: если из таблицы User будет удалён пользователь, то будут удалены все связанные с ним посты.
-# будет создана таблица с этими ячейками
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="Post")
@@ -32,11 +31,11 @@ class Comment(models.Model):
 
 
 class Follow(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name="follower") # который подписывается
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following") # на которого подписываются
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name="follower")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
 
     def __str__(self):
-        return self.user.username  # профиль пользователя
+        return self.user.username
 
     class Meta:
         unique_together = (('user', 'author'), ) 
