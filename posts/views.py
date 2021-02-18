a48b22736322eef1c9a89d8f7c42a8560f7c2d69
Daniil Lapshenkov <lapshenkov.96@mail.ru>
2021-02-18 16:53:10 +0300

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -11,10 +11,9 @@ from django.views.decorators.cache import cache_page
 @cache_page(20)
 def index(request):
     post_list = Post.objects.order_by("-pub_date").all()
-    paginator = Paginator(post_list, 10) # показывать по 10 записей на странице.
-    # Для получения параметров из строки запроса применяется метод request.GET.get().
-    page_number = request.GET.get('page') # переменная в URL с номером запрошенной страницы
-    page = paginator.get_page(page_number) # получить записи с нужным смещением
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
     return render(request, 'index.html', {'page': page, 'paginator': paginator})
 
 
@@ -30,35 +29,35 @@ def group_posts(request, slug):
 @login_required
 def new_post(request):
     if request.method == "POST":
-        form = PostForm(request.POST,  files=request.FILES or None) # request.post содержит данные из полей на сайте
+        form = PostForm(request.POST,  files=request.FILES or None)
         if form.is_valid():
-            post = form.save(commit=False)  # принимаем данные от формы
+            post = form.save(commit=False)
             post.author = request.user
             post.save()
             return redirect(reverse('index'))
-        return render(request, 'new_post.html', {'form': form}) # если is_valid = False
+        return render(request, 'new_post.html', {'form': form})
     form = PostForm()  # пустая форма       
     return render(request, "new_post.html", {"form": form})
 
-# Basically, we have two things here: we save the form with form.save and we add an author (since there was no author field in the PostForm and this field is required). 
-# commit=False means that we don't want to save the Post model yet – we want to add the author first. Most of the time you will use form.save() 
-# without commit=False, but in this case, we need to supply it. post.save() will preserve changes (adding the author) and a new blog post is created! 
 
 def profile(request, username):
-    profile = get_object_or_404(User, username=username) # получаем объект User и колонку, из которой выбираем данные
-    posts = Post.objects.filter(author=profile).order_by("-pub_date").all() # все посты одного автора по ключу
+    profile = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=profile).order_by("-pub_date").all()
     my_posts = Post.objects.filter(author=profile).count()
     paginator  = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    followers = Follow.objects.filter(author=profile).count() # количество подписчиков
-    followed = Follow.objects.filter(user=profile).count() # количество подписок
-    following = Follow.objects.filter(user=request.user.id, author=profile) # if following
-    return render(request, "profile.html", {"profile": profile, 'page': page, 'paginator': paginator, "my_posts": my_posts, "followers": followers, "followed": followed, "following": following})
+    followers = Follow.objects.filter(author=profile).count()
+    followed = Follow.objects.filter(user=profile).count()
+    following = Follow.objects.filter(user=request.user.id, author=profile)
+    return render(request, "profile.html", {"profile": profile, 'page': page, 
+                                            'paginator': paginator, "my_posts": my_posts, 
+                                            "followers": followers, "followed": followed,
+                                            "following": following})
 
 
 def post_view(request, username, post_id):
-    post = get_object_or_404(Post, pk=post_id) # функция get_object_or_404 получает по заданным критериям объект из базы данных или возвращант сообщение об ошибке, если объект не найден \
+    post = get_object_or_404(Post, pk=post_id)
     profile = get_object_or_404(User, username=username)
     my_posts = Post.objects.filter(author=profile).count()
     comments = Comment.objects.filter(post=post).order_by('-created')
@@ -68,11 +67,11 @@ def post_view(request, username, post_id):
 
 @login_required
 def post_edit(request, username, post_id):
-    post = get_object_or_404(Post, pk=post_id) # мы передаём параметр pk из URL-адреса
+    post = get_object_or_404(Post, pk=post_id)
     user = get_object_or_404(User, username=username)
     if request.user != user:
         return redirect("post", post_id=post_id, username=request.user.username)
-    form = PostForm(request.POST or None, files=request.FILES or None, instance=post) # форма со старыми и новыми данными
+    form = PostForm(request.POST or None, files=request.FILES or None, instance=post)
     if request.method == "POST":
         if form.is_valid(): 
             post = form.save(commit=False)
@@ -124,8 +123,6 @@ def profile_unfollow(request, username):
 
 
 def page_not_found(request, exception=None):
-    # Переменная exception содержит отладочную информацию, 
-    # выводить её в шаблон пользователской страницы 404 мы не станем
     return render(request, "misc/404.html", {"path": request.path}, status=404)
 
 def server_error(request):
