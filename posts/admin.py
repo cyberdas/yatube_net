255dc31cc5f8f70fe1e84395b69ef70a45b496d6
Daniil Lapshenkov <lapshenkov.96@mail.ru>
2021-02-18 16:49:13 +0300

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Post, Group
 
-class PostAdmin(admin.ModelAdmin): # перечисляем поля, которые должны отображаться в админке
-    list_display = ("pk", "text", "pub_date", "author")  # добавляем интерфейс для поиска по тексту постов
-    search_fields = ("text",)  # добавляем возможность фильтрации по дате
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "author")
+    search_fields = ("text",)
     list_filter = ("pub_date",)  
-    empty_value_display = '-пусто-'  # это свойство сработает для всех колонок: где пусто - там будет эта строка
+    empty_value_display = '-пусто-'
     def group_title(self, obj):
         return obj.group.title
     group_title.short_description = "Group"
@@ -13,7 +13,7 @@ class PostAdmin(admin.ModelAdmin): # перечисляем поля, котор
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "description")
     empty_value_display = '-пусто-'
-    
-# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin (для отображения к админке)
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
